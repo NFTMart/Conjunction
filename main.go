4a706b28e14d1eaf7216cd3c84b7196ec33dbbf9
Rishi Panthee <rrpanthe@ncsu.edu>
2023-11-28 21:18:06 -0600

    Actually handle cors

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	LoadNodes("nodes.json")
-	r := gin.New()
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
